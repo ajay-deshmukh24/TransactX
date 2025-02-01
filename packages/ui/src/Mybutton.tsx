86256396c0bd09692372ca1627c2c2e35589efa0
ajay-deshmukh24 <ajayadeshmukh2003@gmail.com>
2025-02-01 07:33:42 +0530

    add talwind to project error resolved

---
+++ packages/ui/src/Mybutton.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const Mybutton = () => {
+  return (
+    <button className="px-6 py-3 bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-semibold text-lg rounded-xl shadow-lg hover:from-indigo-600 hover:to-blue-500 transition-all duration-300 transform hover:scale-105 focus:ring-4 focus:ring-blue-300">
+      Click Me
+    </button>
+  );
+};
