392954bc655aecfa5defbdee36e229c725f0dd77
ajay-deshmukh24 <ajayadeshmukh2003@gmail.com>
2025-02-01 10:42:47 +0530

    setup prisma to project

--- packages/ui/src/Mybutton.tsx
+++ packages/ui/src/Mybutton.tsx
@@ -2,7 +2,10 @@
 
 export const Mybutton = () => {
   return (
-    <button className="px-6 py-3 bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-semibold text-lg rounded-xl shadow-lg hover:from-indigo-600 hover:to-blue-500 transition-all duration-300 transform hover:scale-105 focus:ring-4 focus:ring-blue-300">
+    <button
+      className="text-lg px-6 py-3 bg-blue-600 text-black font-semibold rounded-lg transition duration-300 hover:bg-red-700 hover:text-white"
+      onClick={() => alert("Button Clicked!")}
+    >
       Click Me
     </button>
   );
