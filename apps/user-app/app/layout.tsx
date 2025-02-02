5eec286baf1e3e55522a708ffa1538bf29bd648e
ajay-deshmukh24 <ajayadeshmukh2003@gmail.com>
2025-02-02 11:11:28 +0530

    recoil not working project cleaned

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Providers } from "../provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Providers>
-        <body className={inter.className}>{children}</body>
-      </Providers>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
