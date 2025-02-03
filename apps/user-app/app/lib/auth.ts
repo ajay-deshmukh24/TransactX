c4540e9600ed87407c7bbc45a077625babb4467b
ajay-deshmukh24 <ajayadeshmukh2003@gmail.com>
2025-02-03 16:04:32 +0530

    add next-auth to project

---
+++ apps/user-app/app/lib/auth.ts
@@ -0,0 +1,9 @@
+// import { PrismaClient } from "@repo/db/client";
+// import { CredentialsProvider } from "next-auth/providers/credentials";
+// import bcrypt from "bcrypt";
+
+// const client = new PrismaClient();
+
+// export const authOptions = {
+//     providers:[]
+// }
