42a9a3ef810b767dfd709e21babf5d7046b35cfe
ajay-deshmukh24 <ajayadeshmukh2003@gmail.com>
2025-02-04 21:38:28 +0530

    solve env error and check next-auth at client and server side

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,19 +1,28 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@repo/db/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+// import { authOptions } from "../../lib/auth";
+// import { PrismaClient } from "@repo/db/client";
 
-const client = new PrismaClient({
-  log: ["query", "info", "warn", "error"],
-});
+// const client = new PrismaClient({
+//   log: ["query", "info", "warn", "error"],
+// });
 
 export const GET = async () => {
-  await client.user.create({
-    data: {
-      email: "asd",
-      name: "adidas",
-    },
-  });
+  const session = await getServerSession(authOptions);
 
-  return NextResponse.json({
-    message: "hi there",
-  });
+  if (session.user) {
+    return NextResponse.json({
+      user: session.user,
+    });
+  }
+
+  return NextResponse.json(
+    {
+      message: "You are not logged in",
+    },
+    {
+      status: 403,
+    }
+  );
 };
